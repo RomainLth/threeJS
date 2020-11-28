5d8f5e9d93d764a2e5a733e04cea6806e644bf5c
Daoud <daoudhandas@gmail.com>
2020-11-28 11:25:41 +0100

    Ajout de l'index optimise

--- three.js-master/app/index.js
+++ three.js-master/app/index.js
@@ -5,11 +5,14 @@ let camera, scene, renderer, controls;
 const fieldOfView = 75;
 const aspect = window.innerWidth / window.innerHeight;
 const nearClip = 0.1;
-const farClip = 2000;
+const farClip = 20000;
  let towers = [];
  let walls = [];
  let bricks = [];
  let roofs = [];
+ let plane = [];
+ let drops = [];
+ let board = [];
 
   init();
   animate();
@@ -23,6 +26,7 @@ const farClip = 2000;
   function init() {
     // SCENE
     scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x181B3C);
     // CAMERA
     camera = new THREE.PerspectiveCamera(fieldOfView, aspect, nearClip, farClip)
     // RENDU
@@ -34,23 +38,52 @@ const farClip = 2000;
     // Rendu du canvas
     document.body.appendChild(renderer.domElement);
 
+    // Lumière ambiante
+    const ambient = new THREE.AmbientLight( 0xffffff, 0.4 );
+		scene.add( ambient );
+
+    // Terre
+	  let earth = MeshGenerator({
+		  type : 'sphere',
+		  dimension : [750, 750, 750],
+		  texture : 'textures/planenis.jpeg',
+		  position : [-35, -750, 0],
+		  rotation : [100, 50, -110]
+	  })
+	  plane.push(earth);
+
+  	// VLADI
+	  let vladimir = MeshGenerator({
+		  type : "box",
+		  dimension : [100, 50, 10],
+		  texture : 'textures/poutine.png',
+		  position : [100, 20, -150],
+		  rotation : [0, 20, -17]
+	  })
+	  let vladimirCone = MeshGenerator({
+		  type: "cylinder",
+		  dimension: [4, 4, 60, 30],
+		  texture : 'textures/wood.jpg',
+		  position : [100, 0, -150],
+		  rotation : [0, 20, -17]
+	  })
+	  board.push(vladimir);
+	  board.push(vladimirCone);
+
     // MURS
 
     let moon = MeshGenerator({
-     type : 'sphere',
-     dimension : [45, 32, 32],
-     texture : 'textures/moon.jpg',
-     position: [-200, 400, 400]
-  })
-
-      let wallNorth = MeshGenerator({
+	 type : 'sphere',
+	 dimension : [187, 187, 187],
+	 texture : 'textures/moon.jpg',
+	 position: [-200, 400, 2000]
+	})
+    let wallNorth = MeshGenerator({
       type : "box",
       dimension : [220, 30, 20],
       texture : 'textures/brick.png',
       position: [-10, 0, 100]
     })
-
-
     let wallEast = MeshGenerator({
       type : "box",
       dimension : [220, 20, 30],
@@ -58,7 +91,6 @@ const farClip = 2000;
       position: [-150, 0, 0],
       rotation: [90, 0, 90]
     })
-
     let wallWest = MeshGenerator({
       type : "box",
       dimension : [220, 20, 30],
@@ -66,23 +98,18 @@ const farClip = 2000;
       position: [90, 0, 0],
       rotation: [90, 0, 90]
     })
-
     let wallSouthEast = MeshGenerator({
       type : "box",
       dimension : [60, 30, 20],
       texture : 'textures/brick.png',
       position: [-110, 0, -100]
     })
-
     let wallSouthWest = MeshGenerator({
       type : "box",
       dimension : [50, 30, 20],
       texture : 'textures/brick.png',
       position: [50, 0, -100]
     })
-
-
-
 
     walls.push(wallNorth);
     walls.push(wallEast);
@@ -91,42 +118,36 @@ const farClip = 2000;
     walls.push(wallSouthWest);
 
     // MUR TOURS
-
     let wallTower = MeshGenerator({
       type: "cylinder",
       dimension: [25, 25, 50, 30],
       texture : 'textures/brick.png',
       position: [90, 10, -100]
     })
-
     let wallTower2 = MeshGenerator({
       type: "cylinder",
       dimension: [25, 25, 50, 30],
       texture : 'textures/brick.png',
       position: [-145, 10, -100]
     })
-
     let wallTower3 = MeshGenerator({
       type: "cylinder",
       dimension: [25, 25, 50, 30],
       texture : 'textures/brick.png',
       position: [-145, 10, 100]
     })
-
     let wallTower4 = MeshGenerator({
       type: "cylinder",
       dimension: [25, 25, 50, 30],
       texture : 'textures/brick.png',
       position: [90, 10, 100]
     })
-
     let wallTower5 = MeshGenerator({
       type: "cylinder",
       dimension: [18, 18, 50, 30],
       texture : 'textures/brick.png',
       position: [-70, 10, -100]
     })
-
     let wallTower6 = MeshGenerator({
       type: "cylinder",
       dimension: [18, 18, 50, 30],
@@ -148,35 +169,30 @@ const farClip = 2000;
       texture : 'textures/toit.jpg',
       position: [-70, 50, -100]
     })
-
     let roofSouthWest = MeshGenerator({
       type : "cone",
       dimension : [20, 30, 30],
       texture : 'textures/toit.jpg',
       position: [10, 50, -100]
     })
-
     let roofInnerSouthWest = MeshGenerator({
       type : "cone",
       dimension : [22, 30, 30],
       texture : 'textures/toit.jpg',
       position: [40, 50, -40]
     })
-
     let roofInnerSouthEast = MeshGenerator({
       type : "cone",
       dimension : [22, 30, 30],
       texture : 'textures/toit.jpg',
       position: [-95, 50, -40]
     })
-
     let roofInnerNorthWest = MeshGenerator({
       type : "cone",
       dimension : [22, 30, 30],
       texture : 'textures/toit.jpg',
       position: [40, 50, 55]
     })
-
     let roofInnerNorthEast = MeshGenerator({
       type : "cone",
       dimension : [22, 30, 30],
@@ -192,7 +208,6 @@ const farClip = 2000;
     roofs.push(roofInnerNorthEast);
 
     //château
-
     let castle = MeshGenerator({
       type : "cylinder",
       dimension : [40, 40, 80, 50],
@@ -203,7 +218,6 @@ const farClip = 2000;
     towers.push(castle);
 
     //Toit Château
-
     let roofCastle = MeshGenerator({
       type : "cone",
       dimension : [50, 50, 50],
@@ -214,7 +228,6 @@ const farClip = 2000;
     roofs.push(roofCastle);
 
     //PORTAIL
-
     let portal = MeshGenerator({
       type : "box",
       dimension: [50, 47, 5],
@@ -223,352 +236,62 @@ const farClip = 2000;
     })
 
     //DEFENSE NORD
-
-    let brickNorth = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [55, 18, 105]
-    })
-
-    let brickNorth2 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [40, 18, 105]
-    })
-
-    let brickNorth3 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [25, 18, 105]
-    })
-
-    let brickNorth4 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [10, 18, 105]
-    })
-
-    let brickNorth5 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-5, 18, 105]
-    })
-
-    let brickNorth6 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-20, 18, 105]
-    })
-
-    let brickNorth7 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-35, 18, 105]
-    })
-
-    let brickNorth8 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-50, 18, 105]
-    })
-
-    let brickNorth9 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-65, 18, 105]
-    })
-
-    let brickNorth10 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-80, 18, 105]
-    })
-
-    let brickNorth11 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-95, 18, 105]
-    })
-
-    let brickNorth12 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-110, 18, 105]
-    })
+    let posXBrickNorth = 55;
+    for(let i = 0; i < 12; i++)
+    {
+        bricks[i] = MeshGenerator({
+        type: "box",
+        dimension: [8, 8, 8],
+        texture : 'textures/brick.png',
+        position: [posXBrickNorth, 18, 105]
+      })
+      posXBrickNorth = posXBrickNorth -15;
+    }
 
     //DEFENSE EST
-
-    let brickEast = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-155, 18, 65]
-    })
-
-    let brickEast2 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-155, 18, 50]
-    })
-
-    let brickEast3 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-155, 18, 35]
-    })
-
-    let brickEast4 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-155, 18, 20]
-    })
-
-    let brickEast5 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-155, 18, 5]
-    })
-
-    let brickEast6 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-155, 18, -10]
-    })
-
-    let brickEast7 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-155, 18, -25]
-    })
-
-    let brickEast8 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-155, 18, -40]
-    })
-
-    let brickEast9 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-155, 18, -55]
-    })
-
-    let brickEast10 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-155, 18, -70]
-    })
-
-    let brickEast11 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-155, 18, -85]
-    })
-
-    let brickEast12 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-155, 18, -100]
-    })
+    let posZBrickEast  = 65;
+    for(let i = 12; i < 24; i++)
+    {
+        bricks[i] = MeshGenerator({
+        type: "box",
+        dimension: [8, 8, 8],
+        texture : 'textures/brick.png',
+        position: [-155, 18, posZBrickEast]
+      })
+      posZBrickEast = posZBrickEast - 15;
+    }
 
     //DEFENSE OUEST
-
-    let brickWest = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [95, 18, 65]
-    })
-
-    let brickWest2 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [95, 18, 50]
-    })
-
-    let brickWest3 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [95, 18, 35]
-    })
-
-    let brickWest4 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [95, 18, 20]
-    })
-
-    let brickWest5 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [95, 18, 5]
-    })
-
-    let brickWest6 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [95, 18, -10]
-    })
-
-    let brickWest7 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [95, 18, -25]
-    })
-
-    let brickWest8 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [95, 18, -40]
-    })
-
-    let brickWest9 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [95, 18, -55]
-    })
-
-    let brickWest10 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [95, 18, -70]
-    })
-
-    let brickWest11 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [95, 18, -85]
-    })
-
-    let brickWest12 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [95, 18, -100]
-    })
+    let posZBrickWest = 65;
+    for(let i = 24; i < 36; i++)
+    {
+        bricks[i] = MeshGenerator({
+        type: "box",
+        dimension: [8, 8, 8],
+        texture : 'textures/brick.png',
+        position: [95, 18, posZBrickWest]
+      })
+      posZBrickWest = posZBrickWest - 15;
+    }
 
     //DEFENSE SUD
+    let posXBrickSouth = 60;
+    for(let i = 36; i < 42; i++)
+    {
+        if(i == 39)
+        {
+          posXBrickSouth = -89;
+        }
+        bricks[i] = MeshGenerator({
+        type: "box",
+        dimension: [8, 8, 8],
+        texture : 'textures/brick.png',
+        position: [posXBrickSouth, 18, -105]
+      })
+      posXBrickSouth = posXBrickSouth - 13;
+    }
 
-    let brickSouth = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [60, 18, -105]
-    })
 
-    let brickSouth2 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [47, 18, -105]
-    })
-
-    let brickSouth3 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [34, 18, -105]
-    })
-
-    let brickSouth4 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-89, 18, -105]
-    })
-
-    let brickSouth5 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-102, 18, -105]
-    })
-
-    let brickSouth6 = MeshGenerator({
-      type: "box",
-      dimension: [8, 8, 8],
-      texture : 'textures/brick.png',
-      position: [-115, 18, -105]
-    })
-
-    bricks.push(brickNorth);
-    bricks.push(brickNorth2);
-    bricks.push(brickNorth3);
-    bricks.push(brickNorth4);
-    bricks.push(brickNorth5);
-    bricks.push(brickNorth6);
-    bricks.push(brickNorth7);
-    bricks.push(brickNorth8);
-    bricks.push(brickNorth9);
-    bricks.push(brickNorth10);
-    bricks.push(brickNorth11);
-    bricks.push(brickNorth12);
-
-    bricks.push(brickEast);
-    bricks.push(brickEast2);
-    bricks.push(brickEast3);
-    bricks.push(brickEast4);
-    bricks.push(brickEast5);
-    bricks.push(brickEast6);
-    bricks.push(brickEast7);
-    bricks.push(brickEast8);
-    bricks.push(brickEast9);
-    bricks.push(brickEast10);
-    bricks.push(brickEast11);
-    bricks.push(brickEast12);
-
-    bricks.push(brickWest);
-    bricks.push(brickWest2);
-    bricks.push(brickWest3);
-    bricks.push(brickWest4);
-    bricks.push(brickWest5);
-    bricks.push(brickWest6);
-    bricks.push(brickWest7);
-    bricks.push(brickWest8);
-    bricks.push(brickWest9);
-    bricks.push(brickWest10);
-    bricks.push(brickWest11);
-    bricks.push(brickWest12);
-
-    bricks.push(brickSouth);
-    bricks.push(brickSouth2);
-    bricks.push(brickSouth3);
-    bricks.push(brickSouth4);
-    bricks.push(brickSouth5);
-    bricks.push(brickSouth6);
 
     // Tours du fort
 
@@ -647,16 +370,11 @@ const farClip = 2000;
       position: [25, 0, -40]
     })
 
-
-
-
     walls.push(innerWallNorth);
     walls.push(innerWallEast);
     walls.push(innerWallWest);
     walls.push(innerWallSouthEast);
     walls.push(innerWallSouthWest);
-
-
 
     scene.add( moon );
 
@@ -664,47 +382,44 @@ const farClip = 2000;
     scene.add( ...towers );
     scene.add( ...bricks );
     scene.add( ...roofs );
-    scene.add( ...roofs );
     scene.add( portal );
 
+    scene.add( ...plane );
+    scene.add( ...board );
+
+    for (let i = 0; i < 8000; i++) {
+  	 	let stars = MeshGenerator({
+  	 		type: 'cylinder',
+  	 		dimension: [getRandomArbitrary(0, 1), 1],
+  	 		color: '#2196f3',
+  	 		position: [getRandomArbitrary(-2000, 2000), getRandomArbitrary(-2000, 2000), getRandomArbitrary(-2000, 2000)]
+  	 	})
+
+  	 	drops.push(stars); //Implémente rain dans le tableau drops
+  	 }
+
+  	 scene.add(...drops);
 
 
 
-
-
-    // for (var i = 0; i < 2000; i++) {
-  	// 	let rain = MeshGenerator({
-  	// 		type: 'cylinder',
-  	// 		dimension: [getRandomArbitrary(0, 1), 1],
-  	// 		color: '#2196f3',
-  	// 		position: [getRandomArbitrary(-1000, 1000), getRandomArbitrary(0, 900), getRandomArbitrary(-1000, 1000)]
-  	// 	})
-    //
-  	// 	rain.speed = 3 * Math.random();
-    //
-  	// 	drops.push(rain); //Implémente rain dans le tableau drops
-  	// }
-    //
-  	// scene.add(...drops);
-
-    camera.position.z = -600;
+    camera.position.x = 0;
+    camera.position.y = 500;
+    camera.position.z = -500;
 
     //Contrôle de la camera avec la souris
-    controls = new OrbitControls( camera, renderer.domElement);
+    controls = new OrbitControls(camera, renderer.domElement);
     controls.minDistance = 1;
-    controls.maxDistance = 1000;
+    controls.maxDistance = 10000;
 
 }
 
 function animate(){
   requestAnimationFrame(animate);
-
-      renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 
 //Générateur d'objet
 function MeshGenerator(obj) {
-
     let geometry, texture, color, material, mesh;
 
     switch (obj.type.toLowerCase()) {
@@ -720,18 +435,24 @@ function MeshGenerator(obj) {
         case 'cylinder':
             geometry = new THREE.CylinderBufferGeometry(...obj.dimension)
         break;
+		case 'plane' :
+			geometry = new  THREE.PlaneBufferGeometry(...obj.dimension)
+		break;
+		case 'circle' :
+			geometry = new  THREE.CircleBufferGeometry(...obj.dimension)
+		break;
 }
         if(typeof obj.texture !== 'undefined'){
             texture = new THREE.TextureLoader().load( obj.texture );
-            material = new THREE.MeshBasicMaterial({ map: texture  });
+            material = new THREE.MeshPhongMaterial({ map: texture  });
         }
         else if(typeof obj.color !== 'undefined'){
-            material = new THREE.MeshBasicMaterial({
+            material = new THREE.MeshPhongMaterial({
                 color: obj.color
             })
         }
         else {
-            material = new THREE.MeshBasicMaterial();
+            material = new THREE.MeshPhongMaterial();
         }
 
 
